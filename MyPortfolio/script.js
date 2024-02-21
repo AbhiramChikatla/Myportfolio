45d06a6767affbe721e99f27c26faf6d3869b61e
Student <abhiramchikatla03@gmail.com>
2024-02-21 11:57:03 +0530

    added form

---
+++ MyPortfolio/script.js
@@ -0,0 +1,9 @@
+ console.log("hello world")
+ let send_btn=send_form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let username=document.getElementById("Name")
+    let email=document.getElementById("Email")
+    let message=document.getElementById("Query")
+    console.log(username.value,email.value,message.value)
+   
+ }) 
