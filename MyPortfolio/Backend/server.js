f9a5ff5cd5952a5b6d4502d2f66133705ca025ee
AbhiramChikatla <abhiramchikatla03@gmail.com>
2024-02-25 16:46:34 +0530

    fixed backend

--- MyPortfolio/Backend/server.js
+++ MyPortfolio/Backend/server.js
@@ -1,14 +1,43 @@
-import express from "express"
-const app = express()
-const port = 3000
+const express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var nodemailer = require("nodemailer");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.post('/', (req, res) => {
-  res.send('Hello World!')
-})
+// const transporter = nodemailer.createTransport({
+//     port: 465, // true for 465, false for other ports
+//     host: "smtp.gmail.com",
+//     auth: {
+//         user: "abhiramchikatla03@gmail.com",
+//         pass: "Abhi#123$",
+//     },
+//     secure: true,
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Hello World!");
+    // const mailData = {
+    //     from: "abhiramchikatla03@gmail.com", // sender address
+    //     to: "abhiramchikatla03@gmail.com", // list of receivers
+    //     subject: "Msg from the portfolio",
+    //     text: "",
+    //     html: `${req.body}`,
+    // };
+    // transporter.sendMail(mailData, function (err, info) {
+    //     if(err)
+    //       console.log(err)
+    //     else
+    //       console.log(info);
+    //  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
